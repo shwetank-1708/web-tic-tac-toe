74313f835c00f64287421525fa62542c781bfc4b
shwetank-1708 <shwetank.chauhan17@gmail.com>
2024-12-13 15:27:55 +0530

    Added Few New Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import Square from "./Square/Square";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="main-div">
+      <div>
+        <h1 className="game-heading water-background">Tic-Tac-Toe</h1>
+        <div className="square-wrapper">
+          <Square />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Square/Square.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Square.css";
+
+const Square = () => {
+  return <div className="square"></div>;
+};
+
+export default Square;
